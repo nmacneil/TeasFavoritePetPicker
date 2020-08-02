9003be84777faea016acb9d185381edb362b5629
Nathan MacNeil <nate.macneil@gmail.com>
2020-08-02 13:58:34 -0230

    add pets folder with some pets, add file to generate expected structure out of said pets folder for picker

---
+++ createPetJSON.js
@@ -0,0 +1,39 @@
+const fs = require('fs').promises;
+
+async function* ls(path) {
+  const dir = await fs.opendir(path);
+  for await (const dirent of dir) {
+    yield dirent;
+  }
+}
+
+(async () => {
+    const pets = [];
+
+    for await (const petDir of ls('pets')) {
+        const petName = petDir.name;
+
+        for await (const stateDir of ls(`pets/${petName}`)) {
+            const state = stateDir.name;
+
+            const petPath = `pets/${petName}/${state}`;
+
+            for await (const variationDir of ls(petPath)) {
+                const variation = variationDir.name.substring(0, variationDir.name.length - '.png'.length);
+
+                const pet = {
+                    name: variation.split('-')
+                        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+                        .join(' '),
+                    id: `${petDir.name}-${state}-${variation}`,
+                    image: `${petPath}/${variationDir.name}`,
+                    evolved: state === 'evolved',
+                    normal: state === 'normal',
+                };
+                pets.push(pet);
+            }
+        }
+    }
+
+    await fs.writeFile('pets.js', `var pets = ${JSON.stringify(pets, null, 2)}`);
+})();
